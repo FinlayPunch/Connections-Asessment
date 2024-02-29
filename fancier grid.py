4d8b9e3807e6efb72f5e8dae799894d6739eb8df
FinlayPunch <fpunch@student.saintaug.nsw.edu.au>
2024-03-01 08:52:57 +1100

    Added some more categories

--- fancier grid.py
+++ fancier grid.py
@@ -30,12 +30,59 @@ def setup_word_categories():
         'words': ['Charred', 'Beat', 'Leak', 'Pee']
     }
 
+    big_of_liquid_category = {
+        'category_name': 'Bit of liquid',
+        'words': ['Tear', 'Drop', 'Glob', 'Bead']
+    }
+
+    pursue_category = {
+        'category_name': 'Pursue',
+        'words': ['Hunt', 'Stalk', 'Track', 'Trail']
+    }
+
+    eat_a_little_category = {
+        'category_name': 'Eat a little',
+        'words': ['Graze', 'Nibble', 'Peck', 'Snack']
+    }
+
+    ministrone_category = {
+        'category_name': 'Ingredients in minestrone',
+        'words': ['Beans', 'Pasta', 'Stock', 'Vegetables']
+    }
+
+    basic_tastes_category = {
+        'category_name': 'Basic tastes',
+        'words': ['Bitter', 'Salty', 'Sour', 'Sweet']
+    }
+
+    stand_up_to_category = {
+        'category_name': 'Stand up to',
+        'words': ['Brave', 'Confront', 'Face', 'Meet']
+    }
+
+    ilk_category = {
+        'category_name': 'Ilk',
+        'words': ['Kind', 'Sort', 'Type', 'Variety']
+    }
+
+    ism_movements_category = {
+        'category_name': 'Art movements with -ism',
+        'words': ['Manner', 'Expression', 'Romantic', 'Surreal']
+    }
 
 
     word_categories.append(musical_sections_category)
     word_categories.append(party_pooper_category)
     word_categories.append(something_cast_category)
     word_categories.append(vegetable_homophones_category)
+    word_categories.append(big_of_liquid_category)
+    word_categories.append(pursue_category)
+    word_categories.append(eat_a_little_category)
+    word_categories.append(ministrone_category)
+    word_categories.append(basic_tastes_category)
+    word_categories.append(stand_up_to_category)
+    word_categories.append(ilk_category)
+    word_categories.append(ism_movements_category)
 
 
     return word_categories
@@ -65,7 +112,7 @@ def display_game(word_categories, grid_size):
     for j in range(grid_size):
         print(f"|   {j+1}   ", end='')
     print("| ")
-    print((grid_size*4+19)*"-")
+    print((grid_size*4+27)*"-")
 
     # Other rows
     for i in range(grid_size):
@@ -74,7 +121,7 @@ def display_game(word_categories, grid_size):
             random_word = word_categories[random.randint(0, len(word_categories) - 1)]["words"][random.randint(0, 3)] #random word = a random word from a random category from word categories
             print(f"| {random_word:7} ", end='')  # adjusts the width to match the longest word
         print("| ")
-        print((grid_size*4+19)*"-")
+        print((grid_size*4+27)*"-")
 
 
 display_game(word_categories, 4)
