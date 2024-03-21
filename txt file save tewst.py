2bbfb29c3fcab07549314b050479f9e8e99c0143
FinlayPunch <fpunch@student.saintaug.nsw.edu.au>
2024-03-21 11:52:38 +1100

    I am planning to modify this to make it easier for correct guesses and the user's inputs to be compared. The code is simplier than it was before, it can't check whether inputs have been input before, but it can check if user guesses are correct by checking the list against the predetermined correct guesses. Will add file checking functionality later.

--- txt file save tewst.py
+++ txt file save tewst.py
@@ -1,65 +1,48 @@
 playing = True
-GuessIsLetter = False
-import os
+
+correctguesses = {
+    'correctguess1': 'four',
+    'correctguess2': 'five',
+    'correctguess3': 'six',
+    'correctguess4': 'seven',
+}
 
 while playing:
-    while GuessIsLetter != True:
-        Guess1 = input('Guess 1: ')
-        if not Guess1.isalpha():
-            print("Use only letters")
-        if Guess1.isalpha():
-            GuessIsLetter = True
-            GuessIsLetter = False
-            break
-
-    while GuessIsLetter != True:
-        Guess2 = input('Guess 2: ')
-        if not Guess2.isalpha():
-            print("Use only letters")
-        if Guess2.isalpha():
-            GuessIsLetter = True
-            GuessIsLetter = False
-            break
-
-    while GuessIsLetter != True:
-        Guess3 = input('Guess 3: ')
-        if not Guess3.isalpha():
-            print("Use only letters")
-        if Guess3.isalpha():
-            GuessIsLetter = True
-            GuessIsLetter = False
-            break
-
-    while GuessIsLetter != True:
-        Guess4 = input('Guess 4: ')
-        if not Guess4.isalpha():
-            print("Use only letters")
-        if Guess4.isalpha():
-            GuessIsLetter = True
-            GuessIsLetter = False
-            break
-
-   
-    new_guesses = f'Guess 1: {Guess1} - Guess 2: {Guess2} - Guess 3: {Guess3} - Guess 4: {Guess4}\n'
-    try:
-        with open('user_data.txt', 'r') as file:
-            content = file.read()
-            if new_guesses in content:
-                print("You have already entered these guesses.")
+    guesses = []  
+    for i in range(1, 5):
+        while True:
+            guess = input(f"Guess {i}: ")
+            if guess.isalpha():
+                guesses.append(guess)
+                break
             else:
-                with open('user_data.txt', 'a') as file:
-                    file.write(new_guesses)
-                print("Guesses added successfully.")
-    except FileNotFoundError:
-        with open('user_data.txt', 'w') as file:
-            file.write(new_guesses)
-        print("Guesses added successfully.")
+                print("Use only letters")
 
-    print("Do you want to keep going?")
-    choice = input('Yes or no: ')
-    if choice.lower() == 'yes':
-        playing = True
-    elif choice.lower() == 'no':
-        playing = False
-        os.remove("user_data.txt")
-            
+    new_guesses = " - ".join([f"Guess {i}: {guess}" for i, guess in enumerate(guesses, start=1)])
+    print(new_guesses)
+
+
+    correct = True
+    for i, guess in enumerate(guesses, start=1):
+        if guess != correctguesses[f'correctguess{i}']:
+            correct = False
+            break
+
+    if correct:
+        print("your guesses are correct")
+        print("Do you want to keep going?")
+        choice = input('Yes or no: ')
+        if choice.lower() == 'yes':
+            playing = True
+        if choice.lower() == 'no':
+            playing = False
+            break
+    else:
+        print("your guesses aren't correct")
+        print("Do you want to keep going?")
+        choice = input('Yes or no: ')
+        if choice.lower() == 'yes':
+            playing = True
+        if choice.lower() == 'no':
+            playing = False
+            break
